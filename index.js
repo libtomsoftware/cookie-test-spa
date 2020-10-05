1a6d64069f856f6160e90fbd4bd55d3cfd468f8c
Tomasz Libich <t.libich@gmail.com>
2020-10-05 14:10:10 +0100

    Change port 80 to 443

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./app");
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 443;
 
 app.listen(port, () => console.log(`SPA server is listening on port ${port}.`));
